db5b76123d076819c7019abf48574331248704ad
Naël Basset <nael.basset1@gmail.com>
2025-03-09 15:30:11 +0000

    Met à jour la version de Node.js à 18, ajoute un support pour crypto avec des polyfills, et modifie le nom et la version du projet dans package.json.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Pour ajouter des polyfills spécifiques
+      include: ['crypto']
+    }),
+  ],
   server: {
     open: true,
     port: 3000,
     host: '0.0.0.0',
   },
   base: '/coolie/', // Cette configuration semble correcte
+  resolve: {
+    alias: {
+      // Évite les problèmes avec crypto
+      crypto: 'crypto-browserify',
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
